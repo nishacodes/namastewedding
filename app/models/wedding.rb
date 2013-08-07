0c9494f5506306f19f86df899e42c5d069586200
nishacodes <nishabatra27@gmail.com>
2013-08-07 18:14:12 -0400

    removed changes form bates railscast

--- app/models/wedding.rb
+++ app/models/wedding.rb
@@ -1,7 +1,9 @@
 class Wedding < ActiveRecord::Base
   attr_accessible :bridename, :groomname, :url, :weddingcity, :weddingdate, :weddingstate, :user_id
 
+  
   belongs_to :user
-
   validates :user_id, presence: true
+
+	
 end
