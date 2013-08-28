ee58d5bf9ed0c2e1500b40b7ef470084b28fc8c2
nishacodes <nishabatra27@gmail.com>
2013-08-28 12:59:13 -0400

    style changes, new layout for landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
   def home
-  	@inviterequest = Inviterequest.new(params[:inviterequest])
+
+  @inviterequest = Inviterequest.new(params[:inviterequest])
+   if current_user
+    
+   else
+     render :action => 'home', :controller => 'pages'
+   end
   end
 
   def about
